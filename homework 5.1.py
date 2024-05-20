2bece7540fa0705971c4c548199dfcc4bd6001f9
VikaEgorova <vika.egorova97.97@gmail.com>
2024-05-20 19:21:20 +0300

    Add files via upload

---
+++ homework 5.1.py
@@ -0,0 +1,7 @@
+my_dict={'mondey':'понедельник','fryday':'пятница'}
+print(my_dict)
+print(my_dict['mondey'])
+my_dict['mondey']='вторник'
+print(my_dict)
+my_dict.update({'sunday': 'воскресенье'})
+print(my_dict)
---
+++ homework5.py
@@ -0,0 +1,5 @@
+my_list=['apple', 'cherry', 'peach', 'grape']
+print(my_list[0],my_list[3])
+print(my_list[3:5])
+my_list[3]= 'coconat'
+print(my_list)
